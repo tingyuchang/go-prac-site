698f8a2b6fd692ce90d047c8d7bae6b2689abe15
tingyuchang <tingyugenius@gmail.com>
2021-07-20 14:39:25 +0800

    add API CreateUser & Login

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,31 @@
 package models
+
+import "encoding/json"
+
+type User struct {
+	Name string `json:"username"`
+	Password string `json:"password"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+	lastLogin string
+}
+
+func (u User) ToMapData() map[string]interface{} {
+	var returnValue map[string]interface{}
+	marshalValue, _ := json.Marshal(u)
+	json.Unmarshal(marshalValue, &returnValue)
+	return returnValue
+}
+
+
+
+
+func Login(username, password string) (User, error) {
+
+	user := User{
+		Name: username,
+		Password: password,
+	}
+
+	return user, nil
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -27,11 +27,67 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	err := r.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+	username := r.Form.Get("username")
+	password := r.Form.Get("password")
+	email := r.Form.Get("email")
+	phone := r.Form.Get("phone")
+
+	user := &models.User{
+		Name: username,
+		Password: password,
+		Email: email,
+		Phone: phone,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	addressAndTime := fmt.Sprintf("%s_%v", r.RemoteAddr, time.Now())
+	id := fmt.Sprintf("%x", md5.Sum([]byte(addressAndTime)))
+	response := models.Response{
+		Id: id,
+		Data: user.ToMapData(),
+		Datetime: time.Now().Unix(),
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params)  {
+	err := r.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+	username := r.Form.Get("username")
+	password := r.Form.Get("password")
+
+	user, err := models.Login(username, password)
+
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	addressAndTime := fmt.Sprintf("%s_%v", r.RemoteAddr, time.Now())
+	id := fmt.Sprintf("%x", md5.Sum([]byte(addressAndTime)))
+	response := models.Response{
+		Id: id,
+		Data: user.ToMapData(),
+		Datetime: time.Now().Unix(),
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+
+}
+
 
 func NewRouter() *httprouter.Router {
 	router = httprouter.New()
 
 	router.GET("/", Index)
+	router.POST("/user", CreateUser)
+	router.POST("/login", Login)
 
 	return router
 }
